68b5cd104572063a36e74195be59ed18509a86bd
alexsvanissery <alexsvanissery@gmail.com>
2026-02-10 10:24:52 +0530

    use env api url

--- react_library/src/services/api.js
+++ react_library/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API=axios.create({
-    baseURL: "https://library-project-xss6.onrender.com/api/",
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default API;
