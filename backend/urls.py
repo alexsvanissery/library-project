8987833ad1af780e954d36b5e74c2c126079fe3e
alexsvanissery <alexsvanissery@gmail.com>
2026-02-10 11:32:34 +0530

    fix media serving

--- backend/urls.py
+++ backend/urls.py
@@ -1,5 +1,3 @@
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -8,4 +6,5 @@ urlpatterns = [
     path('api/', include('library.urls')),
 ]
 
+# ALWAYS serve media
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
